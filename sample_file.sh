4832a86c0e298151833a016bd47a2401b4fdcec3
sareen_01 <sanya16sareen@gmail.com>
2024-02-18 08:49:58 +0530

    added shell script

---
+++ sample_file.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Create a sample file
+echo "This is a sample file created by the upload_to_s3.sh script." > sample_file.txt
+
+# Upload the file to S3 bucket
+aws s3 cp sample_file.txt s3://uploading-shell-script/sample_file.txt
+
+# Send notification to SQS queue
+aws sqs send-message --queue-url https://sqs.region.amazonaws.com/123456789012/update-on-upload --message-body "File uploaded to S3: sample_file.txt"
